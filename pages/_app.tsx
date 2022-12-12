c6bf675cf016d80941246f38e0ea7f07a47ca365
andresemanueldopazo <andresemanueld@gmail.com>
2022-12-12 03:23:00 -0300

    chore(frontend): add tailwind configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import '../assets/main.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    document.body.classList?.remove('loading')
+  }, [])
+
   return <Component {...pageProps} />
 }
 
